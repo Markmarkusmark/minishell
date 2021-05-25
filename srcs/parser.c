0d8788ebb44ffd419d33930ec081c6c70e097e34
Legate Gunship <legunshi@et-l4.kzn.21-school.ru>
2021-05-25 18:12:21 +0300

    1

--- srcs/parser.c
+++ srcs/parser.c
@@ -28,32 +28,6 @@ char 	*ft_quote(t_all *all, int *i)
 	return (tmp);
 }
 
-char 	*ft_double_quote(t_all *all, int *i)
-{
-	int		j;
-	char	*tmp;
-	char	*tmp2;
-	char	*tmp3;
-
-	j = *i;
-	while (all->str[++(*i)])
-	{
-		if (all->str[*i] == '\\' && (all->str[*i + 1] == '\"' || all->str[*i + 1] == '\'' ||
-		all->str[*i + 1] == '\\' || all->str[*i + 1] == '\"'))
-			str = ft_backslash(str, i);
-		if (all->str[*i] == '\"')
-			break;
-	}
-	tmp = ft_substr(all->str, 0, j);
-	tmp2 = ft_substr(all->str, j + 1, *i - j - 1);
-	tmp3 = ft_strdup(all->str + *i + 1);
-	tmp = ft_strjoin(tmp, tmp2);
-	tmp = ft_strjoin(tmp, tmp3);
-	printf("tmp = %s\n", tmp);
-	free(all->str);
-	return (tmp);
-}
-
 char 	*ft_backslash(t_all *all, int *i)
 {
 	char	*tmp;
@@ -65,6 +39,32 @@ char 	*ft_backslash(t_all *all, int *i)
 	free(all->str);
 	++(*i);
 	printf("tmp = %s\n", tmp);
+	return (tmp);
+}
+
+char 	*ft_double_quote(t_all *all, int *i)
+{
+	int		j;
+	char	*tmp;
+	char	*tmp2;
+	char	*tmp3;
+
+	j = *i;
+	while (all->str[++(*i)])
+	{
+		if (all->str[*i] == '\\' && (all->str[*i + 1] == '\"' || all->str[*i + 1] == '$' ||
+		all->str[*i + 1] == '\\'))
+			all->str = ft_backslash(all, i);
+		if (all->str[*i] == '\"')
+			break;
+	}
+	tmp = ft_substr(all->str, 0, j);
+	tmp2 = ft_substr(all->str, j + 1, *i - j - 1);
+	tmp3 = ft_strdup(all->str + *i + 1);
+	tmp = ft_strjoin(tmp, tmp2);
+	tmp = ft_strjoin(tmp, tmp3);
+	printf("tmp = %s\n", tmp);
+	free(all->str);
 	return (tmp);
 }
 
@@ -90,18 +90,23 @@ void 	ft_parser(t_all *all)
 			all->str = ft_double_quote(all, &i);
 	}
 	printf("----------------------------\n");
-	printf("string after parsing %s\n", str);
+	printf("string after parsing %s\n", all->str);
 }
 
-int 	main(void)
+int 	main(int argc, char **argv, char **env)
 {
+	int 	i;
 	t_all	*all;
 
+	i = -1;
+	while (env[++i])
+		printf("%s\n", env[i]);
+	printf("------------------------------\n");
 	all = (t_all *)malloc(sizeof(t_all));
 	if (!all)
 		close_prog(all, "error memory");
 	ft_bzero(all, sizeof(t_all));
-	all->str = ft_strdup("aaaaaa'dd\\dd'vvvvv\\'vrve'eferfre'4545'666");
+	all->str = ft_strdup("aaaaaa'dd\\dd'vvvvv\\'vr\"ve'efer\"fre'4545'666");
 	printf("input string %s\n", all->str);
 	printf("------------------------------\n");
 	ft_parser(all);
