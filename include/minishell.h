7d7577f3928fe75cd07979d8d2fd77cd082abdba
Марсель Аминов <marselaminov@MacBook-Pro-Marsel.local>
2021-05-27 08:53:49 +0300

    msh

--- include/minishell.h
+++ include/minishell.h
@@ -1,25 +1,22 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "../libft/libft.h"
-#include "../get_next_line/get_next_line.h"
-#include "fcntl.h"
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+# include "../libft/libft.h"
+# include "../get_next_line/get_next_line.h"
+# include "fcntl.h"
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
 typedef struct		s_all
 {
 	char 			**env;
 	char			*str; // my input string
-	char 			**arr1; // split string by ;
-	char 			**arr2; // split by command(it is always s[0]) and args
 }					t_all;
 
 void 	close_prog(t_all *all, char *err);
 void	ft_copyenv(t_all *all, char **env);
-
 
 # endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -9,6 +9,57 @@ char	*get_line(t_all *all)
 	return (str);
 }
 
+//int		return_status(int err)
+//{
+//    if (err == 0)
+//        ft_putstr_fd("syntax error near unexpected token |\n", 1);
+//    if (err == 1)
+//        ft_putstr_fd("syntax error near unexpected token ;\n", 1);
+//    return (0);
+//}
+
+int		limit(t_all *all, int i)
+{
+    int j;
+
+    j = 0;
+    while (i >= 0 && all->str[i] != ';')
+    {
+        if (all->str[i] == '|' && all->str[i + 1] == '|' && j == 0)
+            return (0);
+        if (ft_isalnum(all->str[i]))
+            j++;
+        i--;
+    }
+    return (j == 0 ? 0 : 1);
+}
+
+int		ft_preparse(t_all *all)
+{
+    int		i;
+
+    i = 0;
+    while (all->str[i])
+    {
+        if ((all->str[i] == ';') && !limit(all, i - 1))
+            //return (return_status(1));
+            return (0);
+        else if (all->str[i] == '|' && all->str[i + 1] && all->str[i + 1] == '|')
+        {
+            if (!limit(all, i - 1))
+                //return (return_status(5));
+                return (0);
+            else
+                i++;
+        }
+        else if ((all->str[i] == '|') && !limit(all, i - 1))
+            //return (return_status(0));
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
 int		main(int argc, char **argv, char **env)
 {
 	t_all	*all;
@@ -19,4 +70,6 @@ int		main(int argc, char **argv, char **env)
 	ft_bzero(all, sizeof(t_all));
 	ft_copyenv(all, env);
 	all->str = get_line(all);
+	if (ft_preparse(all))
+
 }
