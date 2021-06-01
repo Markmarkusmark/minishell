0ca0f7be411616847449237683caab344a9e5484
hhhhmryan <75072925+hhhhmryan@users.noreply.github.com>
2021-06-01 17:48:25 +0300

    Add files via upload

--- main2.c
+++ main2.c
@@ -6,7 +6,7 @@
 /*   By: mryan <mryan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 13:36:04 by mryan             #+#    #+#             */
-/*   Updated: 2021/05/30 18:42:00 by mryan            ###   ########.fr       */
+/*   Updated: 2021/06/01 17:47:11 by mryan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,18 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	last_elemenet = ft_lstlast(*lst);
 	last_elemenet->next = new;
 }
+
+void	ft_lstdel(t_list *lst, t_list *lst_main)
+{
+	t_list	*temp;
+
+	temp = lst_main;
+	while (temp->next != lst)
+		temp = temp->next;
+	temp->next = lst->next;
+	free(lst);
+}
+
 int			ft_atoi(char *str)
 {
 	int		res;
@@ -202,12 +214,18 @@ char				**ft_split(char const *s, char c)
 	tab[i] = NULL;
 	return (tab);
 }
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str != c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return ((char*)str);
+}
 ////////////////////
-
-
-
-
-
 
 
 typedef struct      s_env
@@ -236,7 +254,9 @@ typedef struct  s_msh
 	int    fd_1;
 }     t_msh;
 
-
+/*
+	посмотреть позжн echo -n еще раз
+*/
 int bi_echo(char **arg, int fd)
 {
 	int i;
@@ -244,6 +264,14 @@ int bi_echo(char **arg, int fd)
 	i = 0;
 	if (arg[0] == NULL)
 		write(fd, "\n", 2);
+	else if (strcmp(arg[0], "-n") == 0)
+	{
+		while (arg[i] != NULL)
+		{
+			write(fd, arg[i], ft_strlen(arg[i]));
+			i++;
+		}
+	}
 	else
 	{
 		while (arg[i] != NULL)
@@ -256,6 +284,10 @@ int bi_echo(char **arg, int fd)
 	return (0);
 }
 
+/*
+	если после cd нет аргументов, не обрабатываем?
+	изменить PWD в переменной окружения
+*/
 int bi_cd(char **arg, int fd)
 {
 	char s[100];
@@ -263,7 +295,7 @@ int bi_cd(char **arg, int fd)
 	//processes
 	printf("%s\n", getcwd(s, 100));
 	if (arg[0] == NULL)
-		chdir("/Users/mryan");
+		write(1, "wrong path", 10);
 	else
 	{
 		if (chdir(arg[0]) != 0)
@@ -273,6 +305,9 @@ int bi_cd(char **arg, int fd)
 	return (0);
 }
 
+/*
+	мне не нравится делать такой массив, нужно подумать
+*/
 int bi_pwd(char **arg, int fd)
 {
 	char dir[100]; // ?
@@ -283,6 +318,9 @@ int bi_pwd(char **arg, int fd)
 	return (0);
 }
 
+/*
+	работает, но возможно нужно будет скорректировать позже
+*/
 int bi_exit(char **arg, int fd)
 {
 	int n;
@@ -298,6 +336,9 @@ int bi_exit(char **arg, int fd)
 	}
 }
 
+/*
+	в каком виде нужно выводить export без аргументов? уточнить
+*/
 int bi_export(char **arg, t_msh *msh, int fd) // ???????
 {
 	int i = 0;
@@ -333,15 +374,120 @@ int bi_export(char **arg, t_msh *msh, int fd) // ???????
 	return(0);
 }
 
+/*
+*/
+int bi_unset(char **arg, t_msh *msh, int fd)
+{
+	int i = 0;
+	t_env	envp;
+	t_env	*temp;
+	t_list	*lst;
+
+	if (arg[0] != NULL)
+	{
+		msh->env_args = ft_split(arg[i], '=');
+		envp.key = msh->env_args[0];
+    	envp.val = msh->env_args[1];
+		lst = msh->env;
+		while (lst)
+		{
+			temp = lst->content;
+			if (strcmp(temp->key, envp.key) == 0)
+				break;
+			lst = lst->next;
+		}
+		ft_lstdel(lst, msh->env);
+	}
+	return (0);
+}
+
+/*
+	сделать ennro
+	вывод?
+*/
+void print_env(t_msh *msh, int fd)
+{
+	while (msh->env)
+	{
+		t_env	*envp;
+
+		envp = malloc(sizeof(t_env));
+		envp = msh->env->content;
+		write(fd, envp->key, strlen(envp->key));
+		write(fd, "=", 1);
+		if (envp->val != NULL)
+			write(fd, envp->val, strlen(envp->val));
+		write(fd, "\n", 1);
+		msh->env = msh->env->next;
+	}
+}
+
+void env_change(char *arg, t_msh *msh, int fd, t_env *envp)
+{
+	t_env	*temp;
+	t_list	*lst;
+	int		flag = 0;
+
+	lst = msh->env;
+	while (lst)
+	{
+		temp = lst->content;
+		if (strcmp(temp->key, envp->key) == 0)
+		{
+			temp->val = envp->val;
+			flag = 1;
+			break;
+		}
+		lst = lst->next;
+	}
+	print_env(msh, fd);
+	if (flag == 0)
+	{
+		write(fd, arg, ft_strlen(arg));
+		write(fd, "\n", 1);
+	}
+}
+
+int bi_env(char **arg, t_msh *msh, int fd)
+{
+	int i = 0;
+	t_env	envp;
+
+	if (arg[0] == NULL)
+		print_env(msh, fd);
+	else
+	{
+		while (arg[i] != NULL)
+		{
+			msh->env_args = ft_split(arg[i], '=');
+			envp.key = msh->env_args[0];
+    	    envp.val = msh->env_args[1];
+			if (!envp.val)
+			{
+				if (ft_strchr(arg[0], '=') == NULL)
+				{
+					write(fd, "No such file or directory", 25);
+					write(fd, "\n", 1);
+				}
+				else
+					env_change(arg[i], msh, fd, &envp);
+			}
+			else
+				env_change(arg[i], msh, fd, &envp);
+			i++;
+		}
+	}
+	return (0);
+}
+
 int main(int argc, char **argv, char **env)
 {
 	char *arg[] = {
-	"marsel=marsel",
+	"PWD",
 	NULL
 	};
-
 	t_cmd cmd;
-	cmd.cmd = "export";
+	cmd.cmd = "env";
 	cmd.args_str = arg;
 	cmd.n_args = 2;
 
@@ -351,12 +497,18 @@ int main(int argc, char **argv, char **env)
 	t_env ennv1;
 	ennv1.key = "PWD";
 	ennv1.val = "/mryan/minishell";
+	t_env ennv2;
+	ennv2.key = "PATH";
+	ennv2.val = "secret";
 	
 	t_msh msh;
 	msh.cmd = ft_lstnew(&cmd);
 	msh.env = NULL;
 	msh.env = ft_lstnew(&ennv);
 	ft_lstadd_back(&msh.env, ft_lstnew(&ennv1));
+	ft_lstadd_back(&msh.env, ft_lstnew(&ennv2));
+	
+	// начало:
 	if (strcmp(cmd.cmd, "echo") == 0)
 		bi_echo(cmd.args_str, 1);
 	else if (strcmp(cmd.cmd, "cd") == 0) // изменить pwd в переменной окружения
@@ -365,6 +517,10 @@ int main(int argc, char **argv, char **env)
 		bi_pwd(cmd.args_str, 1);
 	else if (strcmp(cmd.cmd, "export") == 0)
 		bi_export(cmd.args_str, &msh, 1);
+	else if (strcmp(cmd.cmd, "unset") == 0)
+		bi_unset(cmd.args_str, &msh, 1);
+	else if (strcmp(cmd.cmd, "env") == 0)
+		bi_env(cmd.args_str, &msh, 1);
 	else if (strcmp(cmd.cmd, "exit") == 0)
 		bi_exit(cmd.args_str, 1);
 	else
