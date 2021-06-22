9ab2120b14dace2900e538ad9bc4e72d740f0690
Legate Gunship <legunshi@et-g6.kzn.21-school.ru>
2021-06-22 18:11:07 +0300

    1

--- srcs/parser.c
+++ srcs/parser.c
@@ -41,17 +41,17 @@ int     ft_check_symbol(t_line_symbol line)
 int     ft_get_num_of_args(t_line_symbol *line)
 {
     int	i;
-    int	n_args;
+    int	num_args;
 
     i = 0;
-    n_args = 0;
+    num_args = 0;
     while (line[i].c)
     {
         if ((line[i].c == ';' || line[i].c == '|') && line[i].flag == 0)
             break ;
         if ((line[i].c == '>' || line[i].c == '<') && line[i].flag == 0)
         {
-            n_args++;
+            num_args++;
             while ((line[i].c == '>' || line[i].c == '<') && line[i].flag == 0)
                 i++;
         }
@@ -59,12 +59,59 @@ int     ft_get_num_of_args(t_line_symbol *line)
         {
             while (ft_check_symbol(line[i]))
                 i++;
-            n_args++;
+            num_args++;
         }
         while (line[i].c == ' ' && line[i].flag == 0)
             i++;
     }
-    return (n_args);
+    return (num_args);
+}
+
+t_line_symbol	*ft_get_clean_line(t_line_symbol *line)
+{
+	int 			i;
+	int 			j;
+	t_line_symbol	*new_line;
+
+	i = 0;
+	j = 0;
+	if (!line)
+		return (NULL);
+	new_line = malloc((ft_mshstrlen(line) + 1) * sizeof (t_line_symbol));
+	if (!new_line)
+		return (NULL);
+	while (line[i].c)
+	{
+		if (!(line[i].flag == 0 && (line[i].c == '"' || line[i].c == '\'')))
+		{
+			new_line[j] = line[i];
+			j++;
+		}
+		i++;
+	}
+	new_line[j].c = '\0';
+	return (new_line);
+}
+
+int 	ft_get_args(t_msh *msh, t_com *cmd, int *i)
+{
+	int 			j;
+	int 			num;
+	t_line_symbol	*line2;
+
+	num = 0;
+	line2 = ft_get_clean_line(msh->line);
+	if (line2 == NULL)
+		return (1);
+	free(msh->line);
+	msh->line = line2;
+	while (msh->line[*i].c == ' ' && msh->line[*i].flag == 0)
+		*i = *i + 1;
+	while (num < cmd->num_args)
+	{
+
+	}
+	return (0); // если успешно завершится
 }
 
 void     ft_parser(t_msh *msh)
@@ -89,5 +136,19 @@ void     ft_parser(t_msh *msh)
 		ft_get_separator(msh, command, 0, &i);
         ft_skip_spaces(msh, &i);
         command->num_args = ft_get_num_of_args(&msh->line[i]);
+        if (command->num_args == 0)
+		{
+        	free(command);
+        	free(new_list);
+			continue ;
+		}
+        else if (command->num_args > 0)
+		{
+			command->args = malloc(command->num_args * sizeof (t_line_symbol *));
+			if (!command->args)
+				close_prog(msh, "malloc error\n");
+		}
+        if (ft_get_args(msh, command, &i))
+			close_prog(msh, "arguments error\n");
     }
 }
